fa06ae4af80b299e06cbe599d9a96b59e4c25287
NYX <NYX@Javans-iMac.local>
2024-01-11 20:51:14 -0500

    Second part of homework

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from . import app
+from flask import request
+from datetime import datetime
+from app import app
 from fake_data.tasks import tasks_list
 
 
@@ -18,3 +20,33 @@ def get_task(task_id):
         if task['id'] == task_id:
             return task
     return {'error':f'Task with the ID {task_id} does not exist'},404
+
+# New Task
+
+@app.route('/tasks',methods=['POST'])
+def create_task():
+    if not request.is_json:
+        return {'error':'Your content-type must be application/json'},400
+    data = request.json
+    required_fields = ['title',"description"]
+    missing_fields = []
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+    if missing_fields:
+        return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
+
+    title = data.get('title')
+    description = data.get("description")
+
+# Create task
+    new_task = {
+        "id" : len(tasks_list) + 1,
+        "title": title,
+        "description": description,
+        "completed": False,
+        "createdAt": datetime.utcnow()
+    }
+
+    tasks_list.append(new_task)
+    return new_task,    201
